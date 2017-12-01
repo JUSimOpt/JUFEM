c352ef544aedd2ecc9a4d1566676a09f10c0712c
mirza <mirza.cenanovic@ju.se>
2017-12-01 16:06:24 +0100

    initial SolveNonLinImplicit

---
+++ SolveNonLinImplicit.m
@@ -0,0 +1,35 @@
+function OUT = SolveNonLinImplicit(model)
+1
+%%
+
+%%
+
+% Process model input. 
+% Input validation
+% Print output to console and log file
+
+
+
+
+% Lopp through steps
+% TODO: Create steps in model
+
+% Loop through Increments
+
+
+
+% Newton Iteration
+
+% Element Loop
+
+% Material
+
+% Assemble
+
+% Solve linear system
+
+OUT = 1;
+end
+
+
+
