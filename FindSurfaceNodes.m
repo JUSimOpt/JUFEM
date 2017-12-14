a2e7f6aa6701fd9ad54117c959dc327748314638
David S <david.samvin@ju.se>
2017-12-14 14:30:10 +0100

    Script to find the nodes on a surface. Used in Nodesets for SurfaceLoad

---
+++ FindSurfaceNodes.m
@@ -0,0 +1,14 @@
+function [outNodes] = FindSurfaceNodes(mesh)
+% find all nodes on a given surface
+% used in SurfaceLoadSet
+% class C3D8_Mesh
+
+nodes = mesh.nodes; % mesh.nodes, all the nodes in the mesh
+xnod = mesh.P(:,1); % mesh.P(:,1), the X coordinates    
+searchIndices = ismember(roundNR(xnod(nodes), 4), roundNR(max(mesh.P(:,1)), 4));
+outNodes = [nodes(searchIndices(:,1),1), nodes(searchIndices(:,2),2), ...
+    nodes(searchIndices(:,3),3), nodes(searchIndices(:,4),4)...
+    nodes(searchIndices(:,5),5), nodes(searchIndices(:,6),6), ...
+    nodes(searchIndices(:,7),7), nodes(searchIndices(:,8),8)];
+
+end
