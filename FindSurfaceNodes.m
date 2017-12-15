f3a70dc9ff7a3d3790a1ab595745290f639990a5
David S <david.samvin@ju.se>
2017-12-15 03:35:07 +0100

    Added search value as an input to the function.

--- FindSurfaceNodes.m
+++ FindSurfaceNodes.m
@@ -1,11 +1,11 @@
-function [outNodes] = FindSurfaceNodes(mesh)
+function [outNodes] = FindSurfaceNodes(mesh, inSearchValue)
 % find all nodes on a given surface
 % used in SurfaceLoadSet
 % class C3D8_Mesh
 
 nodes = mesh.nodes; % mesh.nodes, all the nodes in the mesh
 xnod = mesh.P(:,1); % mesh.P(:,1), the X coordinates    
-searchIndices = ismember(roundNR(xnod(nodes), 4), roundNR(max(mesh.P(:,1)), 4));
+searchIndices = ismember(roundNR(xnod(nodes), 4), roundNR(inSearchValue, 4));
 outNodes = [nodes(searchIndices(:,1),1), nodes(searchIndices(:,2),2), ...
     nodes(searchIndices(:,3),3), nodes(searchIndices(:,4),4)...
     nodes(searchIndices(:,5),5), nodes(searchIndices(:,6),6), ...
