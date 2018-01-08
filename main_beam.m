2c074344dd5bc9d6fc84c8b8837b9d6ad187297b
mirza <mirza.cenanovic@gmail.com>
2018-01-08 08:31:22 +0100

    updated with integration schemes

--- main_beam.m
+++ main_beam.m
@@ -1,4 +1,4 @@
-clear,clc,close all
+%clear,clc,close all
 
 % This is a demo file solving a Hyperelastic model
 % Models are created using the user defined functions in this file. 
@@ -15,15 +15,16 @@ addpath(fullfile('xFigure'))
 
 %% Create Model - User defined
 model.name = 'HansboBeam';
-
-refinements = 1; d = 0;
+% for refinements = [0,1,2,3]
+refinements = 1; 
+d = 0;
 mesh = CreateMesh(refinements,d);
 mesh = CreateSets(mesh);
 mesh = CreateBoundaryConditions(mesh);
 mesh = CreateLoads(mesh);
 
 xfigure
-h = mesh.vizMesh;
+h = mesh.vizMesh('nodenumbers');
 h.patch.FaceColor = 'w';
 light
 1;
@@ -31,29 +32,41 @@ light
 
 
 % Material properties
+% nuu = [0.45,0.475,linspace(0.49,0.499,10)];
+
 E = 200; %GPa
-nu = 0.49;
+nu = 0.3;
 K=E/(3*(1-2*nu));
 mu=E/(2*(1+nu));
 mu1 = 0.5*mu;
 mu2 = mu-mu1;
 materialData(1).Type = 'Neo-Hooke';
 materialData(1).materialFcn = @(gradU)NeoHook3D_2PK(gradU,mu1,K);
+materialData(1).nu = nu;
+materialData(1).E = E;
 
-procedureType = StaticGeneralAnalyis('initialTimeIncrement',1,'timePeriod',1,'minTimeIncrement',1/100,'maxTimeIncrement',1,...
+procedureType = StaticGeneralAnalyis('initialTimeIncrement',1/10,'timePeriod',1,'minTimeIncrement',1/1000,'maxTimeIncrement',1,...
                                      'equilibriumTolerance',1e-3,'maxEquilibriumIterations',30);
 model.step(1) = AnalysisStep('Step-1',procedureType);
 model.step(1).nonLinearGeometry = 1;
 model.step(1).conservativeLoading = 1;
 
-
-% model.UserElement = @(materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter)UserElement_3D_Full(...
-%                      materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter,...
+% 
+% model.UserElement = @(histvar,materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter)UserElement_3D_Full(...
+%                      histvar,materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter,...
 %                      @mesh.BaseFcnParam_Static ); % Default
                  
-model.UserElement = @(materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter)UserElement_3D_Reduced(...
-                     materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter,...
-                     @mesh.BaseFcnParam_Static,@mesh.BaseFcnParam_1Point_Static );
+% model.UserElement = @(histvar,materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter)UserElement_3D_Reduced(...
+%                      histvar,materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter,...
+%                      @mesh.BaseFcnParam_Static);
+
+% model.UserElement = @(histvar,materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter)UserElement_3D_1Point_isoStab(...
+%                      histvar,materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter,...
+%                      @mesh.BaseFcnParam_Static,@mesh.BaseFcnParam_1Point_Static );
+%                  
+model.UserElement = @(histvar,materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter)UserElement_3D_1Point_IsoVolStab(...
+                     histvar,materialData,Xc,ue,ieqs,GP,GW,cInc,time,dTime,iter,...
+                     @mesh.BaseFcnParam_Static,@mesh.BaseFcnParam_1Point_Static );                 
 
 model.integrationOrder = 2;
 
@@ -67,10 +80,21 @@ model.solver = @(model)SolveStaticNonLinImplicit(model,'IterationConvergenceStud
 % TODO inport model from .mat and run through the PreProcessor
 model = PreProcessor(mesh,model,materialData);
 
-vizBCandLoads(model);
+% vizBCandLoads(model);
 
 %% Solver be here
 OUT = model.solver(model);
+filename = ['results//HansboBeam_F_IsoVolStab.mat']
+save(filename,'model','OUT')
+% end
+%% Post Process
+
+
+
+
+
+
+
 
 
 
